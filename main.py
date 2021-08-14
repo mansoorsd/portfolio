efc879a24f88c0de807361ee9efac2acbf8530f4
mansoor <bashasyed0511@gmail.com>
2021-08-14 17:01:10 +0530

    changes

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template("projects.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
